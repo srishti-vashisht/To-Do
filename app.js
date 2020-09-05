af60719d405d94c9d4a1d99a0ad4c034eb2e7d22
Srishti <srishti.vashisht@outlook.com>
2020-09-05 23:21:05 +0530

    Third

--- app.js
+++ app.js
@@ -5,23 +5,32 @@ const app = express();
 
 app.set('view engine','ejs');                              /*specifying that we are using EJS with express */
 
-app.get("/",function(req,res){
+app.use(bodyParser.urlencoded({extended:true}));
 
-    var today = new Date(); 
-    var currentDay = today.getDay();
-    var day ="";                                /*res.render will use the view engine to render a particular page*/
+var items =["Buy Food","Read Books","Excercise"];                                             /*items is an array as we need to save the data that we are adding up in the list */
 
-    if(currentDay===6 || currentDay===0)            /*0 represents Sunday , 6- Saturday*/
-    {
-        day = "Weekend";
-                                         
-    }else{                                      
-        day = "Weekday";
-    }
+app.get("/",function(req,res){                                
+    var today = new Date();                                
+
+    var options= {                                             /*option is javascript object */
+       weekday:"long",
+       day:"numeric",
+       month:"long"
+    }                    
+    
+    day = today.toLocaleDateString("en-us",options);          /*tolocaleDateString() method for javascript date format */
 
 
-    res.render("list",{kindofDay:day});                       /*Render list and passing javascript object which has a key value pair */
+    res.render("list",{kindofDay:day,newListItems:items});             /*res.render will use the view engine to render a particular page*/          /*Render list and passing javascript object which has a key value pair */
 })                                                            /*key value = as defined in .ejs file*/
+                                                              /*when rendering list, pass values of all variables , data you want to send back to website, items array being sent back and rendered on website,along with the day itself*/
+
+app.post("/",function(req,res){
+    var item = req.body.newItem;
+
+    items.push(item);                                       /*appends or add new item (that is received on every post request) in the array items  */
+    res.redirect("/");                                         /*will redirect to home route, app.get("/") for home route and render data on website */
+})
 
 app.listen(3000,function(){
     console.log("Server running on port 3000");
